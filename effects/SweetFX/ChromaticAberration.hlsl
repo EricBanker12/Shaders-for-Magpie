5845c5c5d8e6d15aee00e21b244539e913c3166c
Eric Banker <EricBanker12@gmail.com>
2023-07-26 10:37:59 -0500

    finish porting chromatic aberration

--- effects/SweetFX/ChromaticAberration.hlsl
+++ effects/SweetFX/ChromaticAberration.hlsl
@@ -2,38 +2,66 @@
  * Chromatic Aberration
  * by Christian Cann Schuldt Jensen ~ CeeJay.dk
  *
+ * Ported to Magpie by Eric Banker ~ Kourinn
+ *
  * Distorts the image by shifting each color component, which creates color artifacts similar to those in a very cheap lens or a cheap sensor.
  */
 
-#include "ReShadeUI.fxh"
+//!MAGPIE EFFECT
+//!VERSION 3
+//!OUTPUT_WIDTH INPUT_WIDTH
+//!OUTPUT_HEIGHT INPUT_HEIGHT
 
-uniform float2 Shift < __UNIFORM_SLIDER_FLOAT2
-	ui_min = -10; ui_max = 10;
-	ui_tooltip = "Distance (X,Y) in pixels to shift the color components. For a slightly blurred look try fractional values (.5) between two pixels.";
-> = float2(2.5, -0.5);
-uniform float Strength < __UNIFORM_SLIDER_FLOAT1
-	ui_min = 0.0; ui_max = 1.0;
-> = 0.5;
+//!PARAMETER
+//!LABEL Shift X-axis
+// Distance (X,Y) in pixels to shift the color components. For a slightly blurred look try fractional values (.5) between two pixels.
+//!DEFAULT 2.5
+//!MIN -10.0
+//!MAX 10.0
+//!STEP 0.1
+float ShiftX;
 
-#include "ReShade.fxh"
+//!PARAMETER
+//!LABEL Shift Y-axis
+// Distance (X,Y) in pixels to shift the color components. For a slightly blurred look try fractional values (.5) between two pixels.
+//!DEFAULT -0.5
+//!MIN -10.0
+//!MAX 10.0
+//!STEP 0.1
+float ShiftY;
 
-float3 ChromaticAberrationPass(float4 vpos : SV_Position, float2 texcoord : TexCoord) : SV_Target
-{
-	float3 color, colorInput = tex2D(ReShade::BackBuffer, texcoord).rgb;
+//!PARAMETER
+//!LABEL Strength
+//!DEFAULT 0.5
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float Strength;
+
+//!TEXTURE
+Texture2D INPUT;
+
+//!SAMPLER
+//!FILTER POINT
+SamplerState SamplePoint;
+
+//!SAMPLER
+//!FILTER LINEAR
+SamplerState SampleLinear;
+
+//!PASS 1
+//!DESC Distorts the image by shifting each color component, which creates color artifacts similar to those in a very cheap lens or a cheap sensor.
+//!STYLE PS
+//!IN INPUT
+float3 Pass1(float2 texcoord) {
+	float3 color, colorInput = INPUT.SampleLevel(SamplePoint, texcoord, 0).rgb;
+	float2 pixSize = GetInputPt();
+	float2 Shift = float2(ShiftX, ShiftY);
 	// Sample the color components
-	color.r = tex2D(ReShade::BackBuffer, texcoord + (BUFFER_PIXEL_SIZE * Shift)).r;
+	color.r = INPUT.SampleLevel(SampleLinear, texcoord + pixSize * Shift, 0).r;
 	color.g = colorInput.g;
-	color.b = tex2D(ReShade::BackBuffer, texcoord - (BUFFER_PIXEL_SIZE * Shift)).b;
+	color.b = INPUT.SampleLevel(SampleLinear, texcoord - pixSize * Shift, 0).b;
 
 	// Adjust the strength of the effect
 	return lerp(colorInput, color, Strength);
-}
-
-technique CA
-{
-	pass
-	{
-		VertexShader = PostProcessVS;
-		PixelShader = ChromaticAberrationPass;
-	}
 }
