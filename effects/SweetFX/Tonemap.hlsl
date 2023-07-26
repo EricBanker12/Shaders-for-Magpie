6b60302c92a8dbdfa5630c101913bc20f3699696
Eric Banker <EricBanker12@gmail.com>
2023-07-26 13:47:43 -0500

    port tonemap to magpie

--- effects/SweetFX/Tonemap.hlsl
+++ effects/SweetFX/Tonemap.hlsl
@@ -3,43 +3,99 @@
  * by Christian Cann Schuldt Jensen ~ CeeJay.dk
  */
 
-#include "ReShadeUI.fxh"
+//!MAGPIE EFFECT
+//!VERSION 3
+//!OUTPUT_WIDTH INPUT_WIDTH
+//!OUTPUT_HEIGHT INPUT_HEIGHT
 
-uniform float Gamma < __UNIFORM_SLIDER_FLOAT1
-	ui_min = 0.0; ui_max = 2.0;
-	ui_tooltip = "Adjust midtones. 1.0 is neutral. This setting does exactly the same as the one in Lift Gamma Gain, only with less control.";
-> = 1.0;
-uniform float Exposure < __UNIFORM_SLIDER_FLOAT1
-	ui_min = -1.0; ui_max = 1.0;
-	ui_tooltip = "Adjust exposure";
-> = 0.0;
-uniform float Saturation < __UNIFORM_SLIDER_FLOAT1
-	ui_min = -1.0; ui_max = 1.0;
-	ui_tooltip = "Adjust saturation";
-> = 0.0;
+//!PARAMETER
+//!LABEL Gamma/Midtones
+// Adjust midtones. 1.0 is neutral. This setting does exactly the same as the one in Lift Gamma Gain, only with less control.
+//!DEFAULT 1.0
+//!MIN 0.0
+//!MAX 2.0
+//!STEP 0.01
+float Gamma;
 
-uniform float Bleach < __UNIFORM_SLIDER_FLOAT1
-	ui_min = 0.0; ui_max = 1.0;
-	ui_tooltip = "Brightens the shadows and fades the colors";
-> = 0.0;
+//!PARAMETER
+//!LABEL Exposure
+// Adjust exposure.
+//!DEFAULT 0.0
+//!MIN -1.0
+//!MAX 1.0
+//!STEP 0.01
+float Exposure;
 
-uniform float Defog < __UNIFORM_SLIDER_FLOAT1
-	ui_min = 0.0; ui_max = 1.0;
-	ui_tooltip = "How much of the color tint to remove";
-> = 0.0;
-uniform float3 FogColor < __UNIFORM_COLOR_FLOAT3
-	ui_label = "Defog Color";
-	ui_tooltip = "Which color tint to remove";
-> = float3(0.0, 0.0, 1.0);
+//!PARAMETER
+//!LABEL Saturation
+// Adjust saturation.
+//!DEFAULT 0.0
+//!MIN -1.0
+//!MAX 1.0
+//!STEP 0.01
+float Saturation;
 
+//!PARAMETER
+//!LABEL Bleach
+// Brightens the shadows and fades the colors.
+//!DEFAULT 0.0
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float Bleach;
 
-#include "ReShade.fxh"
+//!PARAMETER
+//!LABEL Defog
+// How much of the color tint to remove.
+//!DEFAULT 0.0
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float Defog;
 
-float3 TonemapPass(float4 position : SV_Position, float2 texcoord : TexCoord) : SV_Target
-{
-	float3 color = tex2D(ReShade::BackBuffer, texcoord).rgb;
+//!PARAMETER
+//!LABEL Fog Color (Red)
+// Which color tint to remove.
+//!DEFAULT 0.0
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float FogColor_Red;
+
+//!PARAMETER
+//!LABEL Fog Color (Green)
+// Which color tint to remove.
+//!DEFAULT 0.0
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float FogColor_Green;
+
+//!PARAMETER
+//!LABEL Fog Color (Blue)
+// Which color tint to remove.
+//!DEFAULT 1.0
+//!MIN 0.0
+//!MAX 1.0
+//!STEP 0.01
+float FogColor_Blue;
+
+//!TEXTURE
+Texture2D INPUT;
+
+//!SAMPLER
+//!FILTER POINT
+SamplerState SamplePoint;
+
+//!PASS 1
+//!DESC Distorts the image by shifting each color component, which creates color artifacts similar to those in a very cheap lens or a cheap sensor.
+//!STYLE PS
+//!IN INPUT
+float3 Pass1(float2 texcoord) {
+	float3 color = INPUT.SampleLevel(SamplePoint, texcoord, 0).rgb;
+	float3 FogColor = float3(FogColor_Red, FogColor_Green, FogColor_Blue);
 	color = saturate(color - Defog * FogColor * 2.55); // Defog
-	color *= pow(2.0f, Exposure); // Exposure
+	color *= pow(2.0, Exposure); // Exposure
 	color = pow(color, Gamma); // Gamma
 
 	const float3 coefLuma = float3(0.2126, 0.7152, 0.0722);
@@ -48,25 +104,16 @@ float3 TonemapPass(float4 position : SV_Position, float2 texcoord : TexCoord) : 
 	float L = saturate(10.0 * (lum - 0.45));
 	float3 A2 = Bleach * color;
 
-	float3 result1 = 2.0f * color * lum;
-	float3 result2 = 1.0f - 2.0f * (1.0f - lum) * (1.0f - color);
+	float3 result1 = 2.0 * color * lum;
+	float3 result2 = 1.0 - 2.0 * (1.0 - lum) * (1.0 - color);
 	
 	float3 newColor = lerp(result1, result2, L);
 	float3 mixRGB = A2 * newColor;
-	color += ((1.0f - A2) * mixRGB);
+	color += ((1.0 - A2) * mixRGB);
 	
 	float3 middlegray = dot(color, (1.0 / 3.0));
 	float3 diffcolor = color - middlegray;
 	color = (color + diffcolor * Saturation) / (1 + (diffcolor * Saturation)); // Saturation
 	
 	return color;
-}
-
-technique Tonemap
-{
-	pass
-	{
-		VertexShader = PostProcessVS;
-		PixelShader = TonemapPass;
-	}
 }
